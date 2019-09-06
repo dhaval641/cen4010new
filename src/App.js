1cff6515303d40ffc5497f619d86ee77306c4db8
dhaval641 <54501750+dhaval641@users.noreply.github.com>
2019-09-06 18:35:03 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './matt.css';
+import Card from 'react-bootstrap/Card'
+import photo from './assets/images/Matthew201909.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Matt() {
+    return (
+        <div className="aboutMatt">
+            <div className="body">
+                <div className="row">
+                    <div className="col-lg-4"></div>
+                    <div className="col-lg-4" style={{ textAlign: "center" }}>
+                        <Card className="image" style={{ width: '30rem' }}>
+                            <Card.Img variant="top" src={photo} />
+                            <Card.Body>
+                                <Card.Title>Matthew Taylor</Card.Title>
+                                <Card.Text>
+                                    Rising junior Computer Science student at Florida Atlantic University.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className="col-lg-4"></div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App;
+export default Matt;
